6173b740e8621cc387c46a3233b780e8d26b2c27
Jacob Albano <Jacob Albano@localhost>
2015-02-04 15:58:52 -0500

    Added Angle approach;

--- Approach.cs
+++ Approach.cs
@@ -8,6 +8,9 @@ namespace Glide
 	/// </summary>
 	public class Approach
 	{
+		private const float DEG = -180 / (float) Math.PI;
+		private const float RAD = (float) Math.PI / -180;
+		
 		/// <summary>
 		/// Approach one value to another by a constant distance scalar.
 		/// </summary>
@@ -18,6 +21,36 @@ namespace Glide
 		public void TowardsWithDecay(ref float target, float to, float amount = 0.1f)
 		{
 			target += (to - target) * amount;
+		}
+		
+		
+		public float Angle(float x, float y, float targetX, float targetY, float currentAngle, float lookEase = 0.3f)
+		{
+			currentAngle *= RAD;
+			float cx = (float) Math.Cos(currentAngle), cy = (float) Math.Sin(currentAngle);
+			float tnormX = targetX - x, tnormY = targetY - y;
+			Normalize(ref tnormX, ref tnormY);
+			
+			cx += (tnormX - cx) * lookEase;
+			cy += (tnormY - cy) * lookEase;
+			
+			var angle = (float) Math.Atan2(cy, cx) * DEG;
+			angle %= 360;
+			if (angle < 0)
+				angle += 360;
+			
+			return angle;
+		}
+		
+		static void Normalize(ref float x, ref float y)
+		{
+			if (x == 0 && y == 0)
+				return;
+			
+			var length = (float) Math.Sqrt(x * x + y * y);
+			var scale = 1f / length;
+			x = x * scale;
+			y = y * scale;
 		}
 	}
 }
